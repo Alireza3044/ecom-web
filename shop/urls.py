c1a494e3bf6315647afa4b836c54bf6f2e5e59ac
Alireza Mohammadi <alireza1384928@gmail.com>
2026-07-28 19:25:10 +0330

    Add cart template and product submittion logic

--- shop/urls.py
+++ shop/urls.py
@@ -6,7 +6,9 @@ app_name = "shop"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:pk>/", views.ProductDetail.as_view(), name="detail"),
-    path("cart/add/<int:product_pk>/", views.cart_add, name="cart-add"),
-    path("cart/reduce/<int:product_pk>/", views.cart_reduce, name="cart-reduce"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("cart/", views.cart, name="cart"),
     path("cart/clear/", views.cart_clear, name="cart-clear"),
+    path("cart/add/<int:pk>/", views.cart_add, name="cart-add"),
+    path("cart/remove/<int:pk>/", views.cart_remove, name="cart-remove"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Sum
 from django.http.response import HttpResponse
 from django.core.paginator import Paginator
 from django.views.generic import DetailView
-from .models import Product
-from .forms import SearchForm
-from .cart import Cart
 import logging
+
+from . import models, forms, cart
 
 logger = logging.getLogger(__name__)
 
 
 def index(request):
-    form = SearchForm(request.POST or None)
+    form = forms.SearchForm(request.POST or None)
     products = []
     n_pages = 12
-    cart = Cart(request)
+    cart_obj = cart.Cart(request)
 
     if request.method == "GET":
-        products = Product.objects.all().order_by('id')
+        products = models.Product.objects.all().order_by('id')
         paginator = Paginator(products, n_pages)
         page = request.GET.get("page")
         page_obj = paginator.get_page(page)
@@ -25,14 +25,14 @@ def index(request):
         context = {
             "page_obj": page_obj,
             "form": form,
-            "c_count": len(cart),
+            "c_count": len(cart_obj),
         }
         return render(request, "shop/index.html", context)
     
     if request.method == "POST":
         if form.is_valid():
             search_term = form.cleaned_data.get("search")
-            products = Product.objects.filter(title__icontains=search_term).order_by('id')
+            products = models.Product.objects.filter(title__icontains=search_term).order_by('id')
         
         paginator = Paginator(products, n_pages)
         page = request.POST.get("page")
@@ -46,37 +46,57 @@ def index(request):
 
 
 class ProductDetail(DetailView):
-    model = Product
+    model = models.Product
     template_name = "shop/product_detail.html"
 
 
-def cart_add(request, product_pk):
+def checkout(request):
+    return HttpResponse()
+
+
+def cart(request):
     if request.htmx:
-        cart = Cart(request)
-        product = get_object_or_404(Product, id=product_pk)
-        cart.add(product.pk)
-        
-        logger.debug(f"Add to cart: id={product.pk}")
-        logger.debug(f"Cart content: {cart.get_cart()}")
-        return HttpResponse(len(cart))
-    return redirect("shop:index")
+        cart_obj = cart.Cart(request)
+        logger.debug(f"cart.Cart content: {cart_obj.cart}")
 
-
-def cart_reduce(request, product_pk):
-    if request.htmx:
-        cart = Cart(request)
-        product = get_object_or_404(Product, id=product_pk)
-        cart.reduce(product.pk)
-
-        logger.debug(f"Reduce from cart: id={product.pk}")
-        logger.debug(f"Cart content: {cart.get_cart()}")
-        return HttpResponse(len(cart))
-    return redirect("shop:index")
+        products = models.Product.objects.filter(pk__in=cart_obj.cart)
+        agg = products.aggregate(total=Sum("price"))
+        context = {
+            "products": products,
+            "count": len(cart_obj),
+            "total": agg["total"],
+        }
+        return render(request, "shop/cart.html", context)
+    return redirect(request.path)
 
 
 def cart_clear(request):
     if request.htmx:
-        Cart(request).clear()
+        cart.Cart(request).clear()
         logger.debug(f"Clear cart content.")
         return HttpResponse(0)
+    return redirect("shop:index")
+
+
+def cart_add(request, pk):
+    if request.htmx:
+        cart_obj = cart.Cart(request)
+        product = get_object_or_404(models.Product, pk=pk)
+        cart_obj.add(product.pk)
+        
+        logger.debug(f"Add to cart: pk={product.pk}")
+        logger.debug(f"cart.Cart content: {cart_obj.cart}")
+        return HttpResponse(len(cart_obj))
+    return redirect("shop:index")
+
+
+def cart_remove(request, pk):
+    if request.htmx:
+        cart_obj = cart.Cart(request)
+        product = get_object_or_404(models.Product, pk=pk)
+        cart_obj.remove(product.pk)
+
+        logger.debug(f"Remove from cart: pk={product.pk}")
+        logger.debug(f"cart.Cart content: {cart_obj.cart}")
+        return HttpResponse(len(cart_obj))
     return redirect("shop:index")
