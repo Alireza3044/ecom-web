9e34dcff377f672649f1987b4fa2a0ac476cf2c6
Alireza Mohammadi <alireza1384928@gmail.com>
2026-07-30 14:10:33 +0330

    Refine the Order model and form

--- shop/forms.py
+++ shop/forms.py
@@ -11,4 +11,4 @@ class OrderForm(forms.ModelForm):
 
     class Meta:
         model = Order
-        fields = ["name", "email", "address", "city", "state", "zipcode"]
+        exclude = ["products"]
--- shop/models.py
+++ shop/models.py
@@ -14,13 +14,14 @@ class Product(models.Model):
 
 
 class Order(models.Model):
-    items = models.CharField(max_length=1000)
+    products = models.ManyToManyField(Product, editable=False)
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=15)
     state = models.CharField(max_length=15)
     zipcode = models.CharField(max_length=5)
+
 
     def __str__(self):
         return self.name
