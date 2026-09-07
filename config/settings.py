c0e8632760a21c074e294a49b872cb95ec97931c
Alireza Mohammadi <alireza1384928@gmail.com>
2026-09-07 20:12:15 +0330

    Set PostgreSQL as db backend

--- config/settings.py
+++ config/settings.py
@@ -82,8 +82,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', '127.0.0.1'),
+        'PORT': config('DB_PORT', '5432'),
+        'CONN_MAX_AGE': 60,
     }
 }
 
