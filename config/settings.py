9fd1b3b72f1c2cc0d56bd56ed2fa200ebd78ece8
Alireza Mohammadi <alireza1384928@gmail.com>
2026-09-07 20:15:39 +0330

    Remove django-htmx app and middleware

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_tailwind_cli',
     'widget_tweaks',
-    'django_htmx',
     'shop',
 ]
 
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
